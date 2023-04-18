f3506fee97608a69ad38e09b9aa84a6dc3a85c2a
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-04-19 00:02:16 +0700

    feat: db constant

--- constants.php
+++ constants.php
@@ -6,6 +6,16 @@ define('PORT', 3306);
 define('PASSWORD', '');
 define('USER_NAME', 'root');
 
+define('ROLE_ADMIN', 'AD');
+define('ROLE_KHACH_HANG', 'KH');
+
 define('TABLE_USER', 'nguoi_dung');
+define('TB_USER_NAME', 'ho_ten');
+define('TB_USER_PASSWORD', 'mat_khau');
+define('TB_USER_EMAIL', 'email');
+define('TB_USER_PHONE', 'so_dien_thoai');
+define('TB_USER_ROLE', 'vai_tro');
+
+
 define('TABLE_CONTACT', 'lien_he');
 define('TABLE_PRODUCTS', 'san_pham');
