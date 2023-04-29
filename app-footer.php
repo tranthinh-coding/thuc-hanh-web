4ddd2a05c59328a32d7e1bd5cb1cf2e571defd27
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-04-29 16:38:58 +0700

    chuyển thành phần footer thành 1 file component

---
+++ app-footer.php
@@ -0,0 +1,32 @@
+<footer class="container">
+  <div class="card">
+    <div class="card-image">
+      <img src="./assets/images/tvthink.jpg" />
+    </div>
+    <h3 class="card-title">Trần Văn Thinh</h3>
+    <p class="card-content">02-12-2001</p>
+    <div class="float-box">
+      <p class="button shadow">20103100350</p>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-image">
+      <img src="./assets/images/tvthink.jpg" />
+    </div>
+    <h3 class="card-title">Phạm Nhật Tân</h3>
+    <p class="card-content">02-12-2002</p>
+    <div class="float-box">
+      <p class="button shadow">20103100350</p>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-image">
+      <img src="./assets/images/tvthink.jpg" />
+    </div>
+    <h3 class="card-title">Bùi Thanh Trà</h3>
+    <p class="card-content">02-12-2001</p>
+    <div class="float-box">
+      <p class="button shadow">20103100350</p>
+    </div>
+  </div>
+</footer>
