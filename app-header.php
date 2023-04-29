84aad6406d38ee6ed3594fc6178f067ad5a00a7f
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-04-29 16:39:33 +0700

    hiển thị tên và nút đăng xuất

--- app-header.php
+++ app-header.php
@@ -1,14 +1,19 @@
 <header>
   <nav class="container">
     <div class="nav-right">
-      <a href="dang-ky.php">
-        <button class="button shadow">Đăng ký</button>
-      </a>
-      <a href="dang-nhap.php">
-        <button class="button">Đăng nhập</button>
-      </a>
+      <?php if (Session::has('ho_ten')) : ?>
+        <?php echo Session::get('ho_ten') ?>
+        <a href="dang-xuat.php">
+          <button class="button shadow">Đăng xuất</button>
+        </a>
+      <?php else : ?>
+        <a href="dang-ky.php">
+          <button class="button shadow">Đăng ký</button>
+        </a>
+        <a href="dang-nhap.php">
+          <button class="button">Đăng nhập</button>
+        </a>
+      <?php endif ?>
     </div>
-  </nav>
-
-  <banner></banner>
+  </nav>=
 </header>
