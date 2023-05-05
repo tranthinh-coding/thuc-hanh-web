56f939d03a414920e8e707dc1e97d73b2ec3d619
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-05-06 02:57:13 +0700

    thanh navbar, menu nhỏ

--- app-header.php
+++ app-header.php
@@ -1,38 +1,158 @@
 <header>
-  <nav class="container">
-    <div class="nav-left">
-      <a href="./">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="font-size: 40px;fill: rgba(0, 0, 0, 1);">
-          <path d="M6 18.573c2.206 0 4-1.794 4-4V4.428L19 7.7v7.43a3.953 3.953 0 0 0-2-.557c-2.206 0-4 1.794-4 4s1.794 4 4 4 4-1.794 4-4V7a.998.998 0 0 0-.658-.939l-11-4A.999.999 0 0 0 8 3v8.13a3.953 3.953 0 0 0-2-.557c-2.206 0-4 1.794-4 4s1.794 4 4 4z"></path>
-        </svg>
-      </a>
-    </div>
+  <div class="top-header">
+    <!-- <div class="container">
+      <input type="text" name="search">
+    </div> -->
+  </div>
 
-    <div class="nav-center">
-      <a href="./" alt="Trang chủ"><i style="font-size: 30px" class='bx bxs-home-heart'></i></a>
-      <a href="./<?php if (Session::has('query')) echo "?query=" . Session::get('query') ?>#san-pham">Sản phẩm</a>
-      <a href="./#lien-he">Liên hệ</a>
-    </div>
+  <div class="header-nav">
+    <nav class="container">
+      <div class="nav-original">
+        <div class="nav-left">
+          <a href="./">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="font-size: 40px;fill: rgba(0, 0, 0, 1);">
+              <path d="M6 18.573c2.206 0 4-1.794 4-4V4.428L19 7.7v7.43a3.953 3.953 0 0 0-2-.557c-2.206 0-4 1.794-4 4s1.794 4 4 4 4-1.794 4-4V7a.998.998 0 0 0-.658-.939l-11-4A.999.999 0 0 0 8 3v8.13a3.953 3.953 0 0 0-2-.557c-2.206 0-4 1.794-4 4s1.794 4 4 4z"></path>
+            </svg>
+          </a>
+        </div>
 
-    <div class="nav-right">
-      <?php if (Session::has('ho_ten')) : ?>
-        <?php echo Session::get('ho_ten') ?>
-        <a href="dang-xuat.php">
-          <button class="button shadow">Đăng xuất</button>
-        </a>
-      <?php else : ?>
-        <a href="dang-ky.php">
-          <button class="button shadow">Đăng ký</button>
-        </a>
-        <a href="dang-nhap.php">
-          <button class="button">Đăng nhập</button>
-        </a>
-      <?php endif ?>
-    </div>
-  </nav>
+        <ul class="nav-center">
+          <li class="nav-item" alt="Trang chủ">
+            <a href="./">Trang chủ</a>
+          </li>
+          <li class="nav-item">
+            <a href="./<?php if (Session::has('query')) echo "?query=" . Session::get('query') ?>#san-pham">Sản phẩm</a>
+            <i class='bx bx-chevron-down'></i>
+
+            <ul class="sidebar-list">
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="?query=&?search=<?php echo Session::get('search') ?>"> Tất cả </a>
+              </li>
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="?query=piano&?search=<?php echo Session::get('search') ?>">
+                  <img src="./assets/images/icons/piano.svg" alt="" /> Đàn piano
+                </a>
+              </li>
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="?query=organ&?search=<?php echo Session::get('search') ?>">
+                  <img src="./assets/images/icons/organ.svg" alt="" /> Đàn organ
+                </a>
+              </li>
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="?query=guitar&?search=<?php echo Session::get('search') ?>">
+                  <img src="./assets/images/icons/guitar.svg" alt="" /> Đàn guitar
+                </a>
+              </li>
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="?query=ukulele&?search=<?php echo Session::get('search') ?>">
+                  <img src="./assets/images/icons/ukulele.svg" alt="" /> Đàn Ukulele
+                </a>
+              </li>
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="?query=kalimba&?search=<?php echo Session::get('search') ?>">
+                  <img src="./assets/images/icons/kalimba.svg" alt="" /> Đàn Kalimba
+                </a>
+              </li>
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="?query=drum&?search=<?php echo Session::get('search') ?>">
+                  <img src="./assets/images/icons/drum.svg" alt="" /> Bộ trống -
+                  Drum
+                </a>
+              </li>
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="?query=am-thanh&?search=<?php echo Session::get('search') ?>">
+                  <img src="./assets/images/icons/speaker.svg" alt="" /> Âm thanh
+                </a>
+              </li>
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="?query=micro&?search=<?php echo Session::get('search') ?>">
+                  <img src="./assets/images/icons/microphone.svg" alt="" />
+                  Micro Nhạc cụ - chuyên nghiệp
+                </a>
+              </li>
+              <li class="sidebar-item">
+                <a class="sidebar-link" href="?query=nhac-cu-dan-toc&?search=<?php echo Session::get('search') ?>">
+                  <img src="./assets/images/icons/harp.svg" alt="" />
+                  Nhạc vụ dân tộc
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="./#lien-he">Liên hệ</a>
+          </li>
+        </ul>
+
+        <div class="nav-right">
+          <?php if (Session::has('ho_ten')) : ?>
+            <?php echo Session::get('ho_ten') ?>
+            <a href="dang-xuat.php">
+              <button class="button shadow">Đăng xuất</button>
+            </a>
+          <?php else : ?>
+            <a href="dang-ky.php">
+              <button class="button shadow">Đăng ký</button>
+            </a>
+            <a href="dang-nhap.php">
+              <button class="button">Đăng nhập</button>
+            </a>
+          <?php endif ?>
+        </div>
+      </div>
+    </nav>
+  </div>
 </header>
 
 <style>
+  .top-header {}
+
+  .header-nav {
+    border-radius: 0 0 36px 36px;
+    /* background-color: var(--theme-color); */
+  }
+
+  header {
+    position: sticky;
+    z-index: 1000;
+    top: 0;
+    width: 100%;
+    min-height: 70px;
+    height: auto;
+    filter: drop-shadow(0 8px 32px rgba(0, 0, 0, 0.03));
+    background: #fff;
+    border-radius: 0 0 36px 36px;
+    /* overflow: hidden; */
+    /* color: #fff; */
+  }
+
+  .nav-original {
+    width: 100%;
+    height: var(--header-nav-height);
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .nav-left {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: fit-content;
+  }
+
+  .nav-left .logo-link {
+    width: 40px;
+  }
+
+  .nav-right {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    height: 100%;
+    width: fit-content;
+  }
+
   .nav-center {
     display: flex;
     height: 100%;
@@ -41,12 +161,50 @@
     justify-content: center;
   }
 
-  .nav-center a {
-    display: block;
-    /* height: 100%; */
+  .nav-center .nav-item {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    position: relative;
     padding: 10px 15px;
   }
-  .nav-center a:hover {
+
+  .nav-center .nav-item:hover {
     text-decoration: underline;
+  }
+
+  .nav-center .nav-item .sidebar-list {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    opacity: 0;
+    visibility: hidden;
+    transform: translateY(30px);
+    width: 300px;
+    border-radius: 8px 20px 20px 20px;
+    padding: 10px;
+    background-color: var(--theme-layout);
+    transition: all 0.25s ease;
+  }
+
+  .nav-center .nav-item:hover .sidebar-list {
+    opacity: 1;
+    visibility: visible;
+    transform: translateY(8px);
+    filter: drop-shadow(0 6px 28px rgba(0, 0, 0, 0.02));
+  }
+
+  .nav-center .nav-item::before {
+    content: '';
+    position: absolute;
+    top: 100%;
+    width: 100%;
+    height: 12px;
+    background-color: transparent;
+  }
+
+  .nav-center .nav-item .sidebar-list .sidebar-item img {
+    display: none;
   }
 </style>
